d5710e303e927d4e86f0177f5ac75baa1f88b807
Ilya Romanenko <mirakulus2017@yandex.ru>
2018-08-24 17:19:11 +0300

    получение и установка валют в конвертере

--- src/calcpage.py
+++ src/calcpage.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
-
+from selenium.common.exceptions import TimeoutException
 from locators import Locators
 from base import Page
 from base import Block
@@ -32,89 +32,63 @@ class ConvertationBlock(Block):
         self._el = WebDriverWait(self._driver, 5)\
             .until(lambda el: self._driver.find_element(*Locators.CONVERTATION_BLOCK))
 
-    def __get_el_title(self):
+    # ------- Методы для внутреннего использования. Должны возвращать WebElement
+    def _get_el_title(self):
+        ''' заголовок блок Конвертация'''
         return self._el.find_element(*Locators.CONVERTATION_BLOCK_TITLE)
-    def __get_el_summa(self):
+    def _get_el_summa(self):
+        ''' Поле для ввода суммы для конвертации'''
         return self._el.find_element(*Locators.CONVERTATION_BLOCK_SUMMA)
-    def __get_el_currency_from(self):
+    def _get_el_currency_from(self):
+        ''' списко с выбором валюты ИЗ которой надо конверитровать '''
         return self._el.find_element(*Locators.CONVERTATION_BLOCK_CURRENCY_FROM)
-    def __get_el_currency_to(self):
+    def _get_el_currency_to(self):
+        ''' списко с выбором валюты В которую надо конверитровать '''
         return self._el.find_element(*Locators.CONVERTATION_BLOCK_CURRENCY_TO)
+    def _get_el_currency_item(self, currency_name):
+        ''' Трехбуквенное имя валюты, которая расположена в списках ИЗ/В.  '''
+        return WebDriverWait(self._driver, 2).until(
+                lambda el: self._el.find_element(
+                    By.XPATH, '//div[@class="visible"]/span[contains(text(), "{0}")]'.format(currency_name)),
+                    message="Указанная валюта '{0}' не найдена. Проверьте, что список открыт, и что валюта в нем существует.".format(currency_name))
+
+    # ------ Свойства для клиентского кода
 
     @property
     def title(self):
         ''' Текст заголовка блока '''
-        return self.__get_el_title().text
+        return self._get_el_title().text
 
     @property
     def summa(self):
         ''' Сумма конвертации '''
-        return self.__get_el_summa().get_attribute('value')
+        return self._get_el_summa().get_attribute('value')
 
     @summa.setter
     def summa(self, value):
         ''' Сумма конвертации '''
-        summa_el = self.__get_el_summa()
+        summa_el = self._get_el_summa()
         summa_el.clear()
         summa_el.send_keys(str(value))
 
     @property
     def currency_from(self):
-        ''' Валюта из которой происходит конвертация '''
-        return self.__get_el_currency_from().find_element(By.TAG_NAME, 'strong').text
+        ''' Валюта ИЗ которой происходит конвертация '''
+        return self._get_el_currency_from().find_element(By.TAG_NAME, 'strong').text
 
     @currency_from.setter
     def currency_from(self, value):
-        ''' Валюта из которой происходит конвертация '''
-        cur_from_el = self.__get_el_currency_from()
-        currency_el = cur_from_el.find_element(By.XPATH, '//span[contains(text(), "{0}")]'.format(value))
-        actions = Actions(self._driver)
-        actions \
-            .move_to_element(cur_from_el) \
-            .click() \
-            .wait_until(EC.presence_of_element_located((By.CSS_SELECTOR, "div .visible")), expiration_time = 2) \
-            .move_to_element(cur_from_el.find_element(By.XPATH, '//span[contains(text(), "{0}")]'.format(value))) \
-            .click() \
-            .wait_until(EC.presence_of_element_located((By.XPATH, '//span[contains(text(), "{0}") and @class="selected"]'.format(value))), expiration_time = 1) \
-            .perform()
+        ''' Валюта ИЗ которой происходит конвертация '''
+        self._get_el_currency_from().click()
+        self._get_el_currency_item(value).click()
 
     @property
     def currency_to(self):
-        ''' Целевая валюта в которую происходит конвертация '''
-        return self.__get_el_currency_to().find_element(By.TAG_NAME, 'strong').text
+        ''' Целевая валюта В которую происходит конвертация '''
+        return self._get_el_currency_to().find_element(By.TAG_NAME, 'strong').text
 
     @currency_to.setter
     def currency_to(self, value):
-        ''' Целевая валюта в которую происходит конвертация '''
-
-        # https://stackoverflow.com/questions/11908249/debugging-element-is-not-clickable-at-point-error
-        cur_to_el = self.__get_el_currency_to()
-        print(value)
-        currency_el = cur_to_el.find_element(By.XPATH, '//span[contains(text(), "{0}")]'.format(value))
-        print(currency_el.get_attribute('innerHTML'))
-
-
-        actions = Actions(self._driver)
-        actions \
-            .move_to_element(cur_to_el) \
-            .click() \
-            .wait_until(EC.presence_of_element_located((By.CSS_SELECTOR, "div .visible")), expiration_time = 2) \
-            .move_to_element(currency_el) \
-            .perform()
-        self._driver.execute_script("arguments[0].scrollIntoView()", cur_to_el.find_element(By.XPATH, '//span[contains(text(), "{0}")]'.format(value)))
-        import time
-        time.sleep(5)
-        currency_el = WebDriverWait(self._driver, 5).until(lambda el: cur_to_el.find_element(By.XPATH, '//span[contains(text(), "{0}")]'.format(value)))
-        currency_el.click()
-        # actions = Actions(self._driver)
-        # actions \
-        #     .move_to_element(cur_to_el) \
-        #     .click() \
-        #     .wait_until(EC.presence_of_element_located((By.CSS_SELECTOR, "div .visible")), expiration_time = 2) \
-        #     .move_to_element(cur_to_el.find_element(By.XPATH, '//span[contains(text(), "{0}")]'.format(value))) \
-        #     .wait(1) \
-        #     .click() \
-        #     .perform()
-        # .move_to_element(currency_el).click() \
-            # .wait_until(EC.presence_of_element_located((By.XPATH, '//span[contains(text(), "{0}") and @class="selected"]'.format(value))), expiration_time = 2) \
-
+        ''' Целевая валюта В которую происходит конвертация '''
+        self._get_el_currency_to().click()
+        self._get_el_currency_item(value).click()
